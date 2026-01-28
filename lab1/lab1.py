df1ddd57b03d853ca6e71fb46f5b942ec6880efc
shahirahere5 <139383955+shahirahere5@users.noreply.github.com>
2026-01-28 10:59:02 +0500

    Add files via upload

---
+++ lab1/lab1.py
@@ -0,0 +1,37 @@
+print('hello world')
+name = input('enter name ')
+print(name)
+
+batch = 2024
+if batch == 2024:
+    print('2024')
+else:
+    print('yo')
+
+str1 = 'shahira'
+for i in range(len(str1)):
+    print(str1[i], end='')
+
+set1 = {0,1,2,3,4,5}
+set2 = {5,6,7,8,9,10}
+print(set1 & set2)
+
+dict1 = {
+    "id" : "24k-0639",
+    "name" : 'shahira',
+    'batch':2025
+}
+
+dict1["id"]
+dict1.values()
+dict1['uni'] = 'fast'
+dict1
+del dict1["name"]
+
+list_keys = []
+dict2={
+     
+}
+for i in range(3):
+    list_keys = input("enter key values")
+
